949275869b9da5f2486e5c321ac2a178cbabd304
ShinjiHirano <hirano@isit.or.jp>
2025-12-02 11:22:10 +0900

    htto to orion

--- cityos_subsc/app/env_temp.py
+++ cityos_subsc/app/env_temp.py
@@ -4,4 +4,4 @@ API_VERSION = '1.0.0'
 
 es_server = 'http://cityos_es:9200'
 
-orion_server = 'https://orion:1026'
+orion_server = 'http://orion:1026'
--- cityos_support/app/env_temp.py
+++ cityos_support/app/env_temp.py
@@ -4,7 +4,7 @@ API_VERSION = '1.0.0'
 
 es_server = 'http://cityos_es:9200'
 
-orion_server = 'https://orion:1026'
+orion_server = 'http://orion:1026'
 
 mqtt_broker = 'cityos_emqx'     # コンテナ間通信なので、コンテナ名を指定する
 mqtt_port = 1883
