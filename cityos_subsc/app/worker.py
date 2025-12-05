906fdd691397d9d3c89ddde0cae00e3f7e6f849d
ShinjiHirano <hirano@isit.or.jp>
2025-12-05 15:04:18 +0900

    convert geometry info from orion to es

--- cityos_subsc/app/worker.py
+++ cityos_subsc/app/worker.py
@@ -109,8 +109,6 @@ class Worker(threading.Thread):
     def save_data(self, myconfig, data):
         result = False
         try:
-            #print('myconfig', myconfig)
-            #print('data', data)
             # 履歴DB登録情報があれば、履歴DBに登録する
             if 'key' in myconfig and 'es' in myconfig['key'] and 'fields' in myconfig['key']['es']:
                 doc_id = myconfig['apiname']
@@ -119,8 +117,6 @@ class Worker(threading.Thread):
                     doc_id = f'{doc_id}.{data[field]}'
 
                 data_fields = myconfig['fields']
-                #print('fields', data_fields)
-
                 doc = {}
                 for field in data_fields:
                     if field in data:
